26c837e42003316b59808f97030e788338ab1cb3
mostafa <mostafatam79@gmail.com>
2026-01-07 20:50:08 +0200

    update rate limit

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import rateLimit from 'express-rate-limit';
 const server = express();
 const limiter = rateLimit({
   windowMs: 60 * 1000,
-  max: 100,
+  max: 500,
   standardHeaders: true,
   legacyHeaders: false,
   message: {
